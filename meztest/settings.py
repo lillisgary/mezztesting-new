abd9355b54533b6e8ff8d515c95a9bb418f603fd
lillisgary <lillisgary@gmail.com>
2015-09-16 13:39:08 +0200

    removed sue

--- meztest/settings.py
+++ meztest/settings.py
@@ -204,7 +204,6 @@ TEMPLATE_DIRS = (os.path.join(PROJECT_ROOT, "templates"),)
 ################
 
 INSTALLED_APPS = (
-    "sue",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
